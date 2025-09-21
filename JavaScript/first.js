19846625b50bc8dab4a154e7213ee7f446945136
Pranav Kumar <pranavmehta192@gmail.com>
2025-09-21 12:07:41 +0530

    initial commit

--- JavaScript/first.js
+++ JavaScript/first.js
@@ -1,11 +1,19 @@
 // console.log("pranav kumar");
 // console.log("Hyy there")
 
-fullname = "pranav kumar";
-age = 24;
-console.log(age);
-x = null;
-console.log(x);
-y = undefined;
-console.log(y);
-console.log(fullname);
+// fullname = "pranav kumar";
+// age = 24;
+// console.log(age);
+// x = null;
+// console.log(x);
+// y = undefined;
+// console.log(y);
+// console.log(fullname);
+// console.log(typeof age);
+// console.log(typeof x);
+pranav = "Isha singh";
+console.log(pranav);
+
+
+
+

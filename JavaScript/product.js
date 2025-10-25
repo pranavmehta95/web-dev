88c9990c907304f1299b8353819f23cec45d2cd7
Pranav Kumar <pranavmehta192@gmail.com>
2025-10-25 22:54:01 +0530

    initial commit

---
+++ JavaScript/product.js
@@ -0,0 +1,31 @@
+// fullname = "Pranav Kumar";
+// FULLNAME = "PRANAV KUMAR";
+// let name = "Isha singh";
+// console.log(name);
+// console.log(fullname);
+// console.log(FULLNAME);
+// {
+//     let a = "hello";
+//     console.log(a);
+// }
+// {
+//     // let a = "There is some changes";
+//     // console.log(a);
+// }
+// let a = "There is some changes";
+// const student = {
+//     name: "Isha Singh",
+//     age: 21,
+//     collage: "DU",
+//     cgpa: 8.1,
+// };
+// student.age = student.age + 1;
+// //or
+// // student["age"] = student["age"] + 1;
+// console.log(student.age);
+const product = {
+    title: "Ball Pen",
+    rating: 4.2,
+    offer: 5,
+    price: 270,
+};

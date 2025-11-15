ed38e5ca0fb6e5b3e26c29df5d56aabd60a55751
pranavmehta95 <pranavmehta192@gmail.com>
2025-11-16 01:18:26 +0530

    Started learning DOM

---
+++ DOM/script.js
@@ -0,0 +1,33 @@
+// console.log(window);
+
+// console.dir(window);
+
+// console.dir(document.body);
+
+// document.getElementById("heading");
+// console.dir(heading);
+
+// let button = document.getElementById("myId");
+// console.dir(button);
+
+
+// let headings = document.getElementsByClassName("myClass");
+// console.dir(headings);
+// console.log(headings);
+
+
+// let paras = document.getElementsByTagName("p");
+// // console.log(paras);
+// console.dir(paras);
+
+// let firstEl = document.querySelector("#myId");
+// console.dir(firstEl);
+
+// let allEl = document.querySelectorAll("#myId");
+// console.dir(allEl);
+
+
+// console.dir(document.body.firstChild);
+
+let div = document.querySelector("div");
+console.dir(div);
